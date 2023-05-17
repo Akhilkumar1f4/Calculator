18f2b5ed6418e0863a8347755e6b741f34a90df0
“Akhilkumar1f4” <akhilkumar1f4@gmail.com>
2023-05-18 00:09:27 +0530

    [Akhilkumar1f4]~Subtraction file added

---
+++ Subtraction/Program.cs
@@ -0,0 +1,7 @@
+﻿int a, b, result;
+Console.WriteLine("enter first number");
+a = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine("enter second number");
+b = Convert.ToInt16(Console.ReadLine());
+result = a - b;
+Console.WriteLine(result);
